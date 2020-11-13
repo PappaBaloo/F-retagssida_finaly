c751836ce8068d80a5af2e74a4b4ef9bd3352bdc
Charlie Sesser Nylund <charlie.sessernylun@learnet.se>
2020-11-13 09:35:48 +0100

    fixi doodly

--- funky.js
+++ funky.js
@@ -2,23 +2,19 @@ const burgerSlide = () =>{
     const burger = document.querySelector('.burger');
     const directory = document.querySelector('.directory');
     const directoryLinks = document.querySelectorAll('.directory li');
-//togglar burgern
+      //togglar burgern
     burger.addEventListener('click', ()=>{
-        directory.classList.toggle('directory-active');
-
+        directory.classList.toggle('directory-active')
         //animering av länkarna
         directoryLinks.forEach((link, index) => {
             if (link.style.animation) {
                 link.style.animation = ''
             }
             else{
-            link.style.animation = `directoryfade 0.3s ease forwards ${index / 5 + 0.6}s`
+            link.style.animation = `directoryfade 0.3s ease forwards ${index / 5 + 0.2}s`
             }
             });
-
-//burgare animering
-burger.classList.toggle('trans');
-
-});}
+             //burgare animering
+             burger.classList.toggle('trans');});}
 
 burgerSlide();
