ef8204ced8ad2ac49b14fa255239440f10492ff3
Charlie Sesser Nylund <charlie.sessernylun@learnet.se>
2020-11-22 00:39:51 +0100

    snart är allt klart

--- funky.js
+++ funky.js
@@ -1,20 +1,23 @@
-const burgerSlide = () =>{
-    const burger = document.querySelector('.burger');
-    const directory = document.querySelector('.directory');
-    const directoryLinks = document.querySelectorAll('.directory li');
-      //togglar burgern
-    burger.addEventListener('click', ()=>{
-        directory.classList.toggle('directory-active')
-        //animering av länkarna
-        directoryLinks.forEach((link, index) => {
-            if (link.style.animation) {
-                link.style.animation = ''
-            }
-            else{
-            link.style.animation = `directoryfade 0.3s ease forwards ${index / 5 + 0.2}s`
-            }
-            });
-             //burgare animering
-             burger.classList.toggle('trans');});}
+const burgerSlide = () => {
+  const burger = document.querySelector(".burger");
+  const directory = document.querySelector(".directory");
+  const directoryLinks = document.querySelectorAll(".directory li");
+  //togglar burgern
+  burger.addEventListener("click", () => {
+    directory.classList.toggle("directory-active");
+    //animering av länkarna
+    directoryLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `directoryfade 0.3s ease forwards ${
+          index / 5 + 0.2
+        }s`;
+      }
+    });
+    //burgare animering
+    burger.classList.toggle("trans");
+  });
+};
 
 burgerSlide();
